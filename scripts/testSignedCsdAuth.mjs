c43064235fc2869f07b7837c9652a09942475a22
intervalplace <julnerapple@hotmail.com>
2026-06-07 14:06:41 +0200

    Update testSignedCsdAuth.mjs

--- scripts/testSignedCsdAuth.mjs
+++ scripts/testSignedCsdAuth.mjs
@@ -58,6 +58,8 @@ const signature = await account.signTypedData({
   message: authorization,
 });
 
+authorization.usdcAmount = "2000000";
+
 const res = await fetch(`${AON}/v1/authorizations/csd-usdc/from-signed-auth`, {
   method: "POST",
   headers: { "content-type": "application/json" },
